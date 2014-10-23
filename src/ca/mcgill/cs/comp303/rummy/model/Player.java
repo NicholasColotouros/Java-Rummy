0c1fe1e44e1ebf2c5a9f68e0d11b15d5e604364c
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2014-10-22 22:01:00 -0400

    Partially implemented player

---
+++ src/ca/mcgill/cs/comp303/rummy/model/Player.java
@@ -0,0 +1,40 @@
+package ca.mcgill.cs.comp303.rummy.model;
+
+/**
+ * Represents a player that plays the game.
+ */
+public class Player
+{
+	private final String aName;
+	private int aScore;
+	private Hand aHand;
+	
+	/**
+	 * Default constructor.
+	 * @param pName name of the player
+	 */
+	public Player(String pName)
+	{
+		aName = pName;
+		aScore = 0;
+		aHand = new Hand();
+	}
+	
+	/**
+	 * Gets the name of the player.
+	 * @return the name of the player.
+	 */
+	public String getName()
+	{
+		return aName;
+	}
+	
+	/**
+	 * Returns the players score.
+	 * @return the players score.
+	 */
+	public int getScore()
+	{
+		return aScore;
+	}
+}
