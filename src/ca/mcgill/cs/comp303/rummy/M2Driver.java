6a3a5228397d8e57740d03bc350046da1c1293b3
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2014-10-23 18:48:43 -0400

    Added the driver

---
+++ src/ca/mcgill/cs/comp303/rummy/M2Driver.java
@@ -0,0 +1,22 @@
+package ca.mcgill.cs.comp303.rummy;
+
+/**
+ * The driver for M2 which simulates games of Rummy with two AIs.
+ */
+public class M2Driver
+{
+	/**
+	 * Takes as input an integer and plays that many games against the AI.
+	 * @param args the number of games to be played against the AI
+	 */
+	public static void main(String[] args)
+	{
+		int numGames = Integer.parseInt(args[1]);
+		
+		for(int i = 0; i < numGames; i++)
+		{
+			// TODO: play the games
+			
+		}
+	}
+}
