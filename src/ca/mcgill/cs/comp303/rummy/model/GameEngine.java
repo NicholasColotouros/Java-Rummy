bad7b9eccde9512331d1e5ffadc601281a22c1a6
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2014-10-22 21:45:31 -0400

    implemented save/load methods in GameEngine

--- src/ca/mcgill/cs/comp303/rummy/model/GameEngine.java
+++ src/ca/mcgill/cs/comp303/rummy/model/GameEngine.java
@@ -1,5 +1,9 @@
 package ca.mcgill.cs.comp303.rummy.model;
 
+import ca.mcgill.cs.comp303.rummy.exceptions.LoadException;
+import ca.mcgill.cs.comp303.rummy.exceptions.SaveException;
+import ca.mcgill.cs.comp303.rummy.serialization.Serializer;
+
 /**
  * The heart and soul of the game. Responsible for managing the game.
  */
@@ -15,5 +19,27 @@ public final class GameEngine
 	public static GameEngine getEngine()
 	{
 		return INSTANCE;
+	}
+	
+	/**
+	 * Saves the game.
+	 * @param pSerializer the format for which to implement the serializer
+	 * @param pPath the full path to save the game
+	 * @throws SaveException thrown when something goes wrong during saving 
+	 */
+	public void save(Serializer pSerializer, String pPath) throws SaveException
+	{
+		pSerializer.save(this, pPath);
+	}
+	
+	/**
+	 * Loads the game.
+	 * @param pSerializer the format for which to implement the serializer
+	 * @param pPath the full path to load the game
+	 * @throws LoadException thrown when something goes wrong during loading 
+	 */
+	public void load(Serializer pSerializer, String pPath) throws LoadException
+	{
+		pSerializer.load(pPath);
 	}
 }
