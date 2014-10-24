ff28f163d7a15c94f4fe041296fb8a4d4508b2db
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2014-10-24 18:03:40 -0400

    deleted unused code and save file

--- src/ca/mcgill/cs/comp303/rummy/serialization/JSonSerializer.java
+++ src/ca/mcgill/cs/comp303/rummy/serialization/JSonSerializer.java
@@ -1,10 +1,8 @@
 package ca.mcgill.cs.comp303.rummy.serialization;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
