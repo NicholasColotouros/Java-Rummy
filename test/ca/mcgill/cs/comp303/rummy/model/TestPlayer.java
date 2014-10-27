6bc162a147f28fd6d87973e0b52fdc77ede8382c
kxsong92 <kxsong92@gmail.com>
2014-10-26 20:36:12 -0400

    TestPlayer

--- test/ca/mcgill/cs/comp303/rummy/model/TestPlayer.java
+++ test/ca/mcgill/cs/comp303/rummy/model/TestPlayer.java
@@ -1,5 +1,5 @@
 package ca.mcgill.cs.comp303.rummy.model;
-package ca.mcgill.cs.comp303.rummy.exceptions.*;
+import ca.mcgill.cs.comp303.rummy.exceptions.*;
 
 import static org.junit.Assert.assertTrue;
 
@@ -17,6 +17,8 @@ public class TestPlayer
 	private Player aP1;
 	private Card aC1;
 	private Card aC2;
+	private Card aC3;
+	private Card aC4;
 	private Hand aH1;
 	/**
 	 * 
@@ -24,9 +26,11 @@ public class TestPlayer
 	@Before
 	public void setUp()
 	{
-		aP1 = new Player("Smart Ass");
+		aP1 = new Player("SmartAss");
 		aC1 = new Card(Card.Rank.FIVE, Card.Suit.DIAMONDS);
 		aC2 = new Card(Card.Rank.FIVE, Card.Suit.DIAMONDS);
+		aC3 = new Card(Card.Rank.ACE, Card.Suit.SPADES);
+		aC4 = new Card(Card.Rank.ACE, Card.Suit.CLUBS);
 		aH1.add(aC1);
 				
 	}
@@ -49,6 +53,58 @@ public class TestPlayer
 		{
 			assertTrue(aP1.getHand().equals(aH1));
 		}
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testDiscard()
+	{	
+		try
+		{
+			aP1.discard(aC2);	
+		}
+		catch(CannotDiscardException e)
+		{
+			fail(CannotDiscardException);
+		}
+		finally
+		{
+			assertTrue(aP1.getHand().size() == 0);
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testDiscardHand()
+	{	
+		
+		aP1.addCard(aC2);	
+		aP1.addCard(aC3);	
+		aP1.addCard(aC4);	
+
+		assertTrue(aP1.getHand().size() == 0);		
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testGetName()
+	{	
+		assertTrue(aP1.getName() == "SmartAss");		
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testGetScore()
+	{	
+		assertTrue(aP1.getScore() == 0);		
 	}
 	
 }
