df95d87483bcebc3f7307b47b8c5aaef22314999
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2014-10-28 23:50:13 -0400

    implemented the file logger

--- src/ca/mcgill/cs/comp303/rummy/logging/FileLogger.java
+++ src/ca/mcgill/cs/comp303/rummy/logging/FileLogger.java
@@ -1,8 +1,11 @@
 package ca.mcgill.cs.comp303.rummy.logging;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import ca.mcgill.cs.comp303.rummy.exceptions.DataException;
 
@@ -11,28 +14,43 @@ import ca.mcgill.cs.comp303.rummy.exceptions.DataException;
  */
 public class FileLogger implements ILoggerObserver
 {
-	Logger aLogger;
+	private Logger aLogger;
+	private FileHandler aFH;
+
 	
 	/**
 	 * Logs to the specified file. If the file does not exist, it will be created.
 	 * If the directory does not exist, an exception is thrown.
 	 * @param pPath the complete path to save the file.
+	 * @param pName the name of the logger
 	 * @throws DataException if the directory does not exist.
 	 */
-	public FileLogger(String pPath) throws DataException
+	public FileLogger(String pPath, String pName) throws DataException
 	{
-		// TODO: make sure directory is valid.
-		File file = new File(pPath);
-		if(file.isDirectory())
+		try
 		{
-			throw new DataException("Invalid file name.");
+			// TODO: make sure directory is valid.
+			File file = new File(pPath);
+			if(file.isDirectory())
+			{
+				throw new DataException("Invalid file name.");
+			}
+			if(file.exists())
+			{
+				file.delete();
+			}
+			
+			aLogger = Logger.getLogger(pName);
+			aLogger.addHandler(aFH);
+	        SimpleFormatter formatter = new SimpleFormatter();  
+	        aFH.setFormatter(formatter);
+	        
+	        aLogger.info(pName + ": logger initialized.");
 		}
-		if(file.exists())
-		{
-			file.delete();
-		}
-		
-		// TODO initialize observer
+		catch (SecurityException e) 
+		{  
+	        e.printStackTrace();  
+	    } 
 	}
 
 	@Override
