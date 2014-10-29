47bce9dcdca9dea25644c507343f85d85c168c1e
yuki0715 <improving@yeah.net>
2014-10-28 20:48:54 -0400

    small change on logging

--- src/ca/mcgill/cs/comp303/rummy/logging/Logger.java
+++ src/ca/mcgill/cs/comp303/rummy/logging/Logger.java
@@ -2,8 +2,14 @@ package ca.mcgill.cs.comp303.rummy.logging;
 
 import java.util.logging.*;
 
-public class Logger
-{
-	
-	//private static final Logger LOGGER = Logger.getLogger(MyClass.class.getName()); 
+
+public class Logger implements ILoggerObserver
+	{
+
+		@Override
+		public void updateLoggerObservers(Level pPriority, String pMessage)
+		{
+			
+			
+		}
 }
