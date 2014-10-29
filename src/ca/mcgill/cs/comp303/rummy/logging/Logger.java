0fef4c5397e3464bed52c680d85fcc9e3b45ac90
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2014-10-28 20:42:26 -0400

    Revert "Revert "commented out unworking code, refactored""
    This reverts commit 1df12d955e423b4475020104cc16ade7c63f66d5.

---
+++ src/ca/mcgill/cs/comp303/rummy/logging/Logger.java
@@ -0,0 +1,9 @@
+package ca.mcgill.cs.comp303.rummy.logging;
+
+import java.util.logging.*;
+
+public class Logger
+{
+	
+	//private static final Logger LOGGER = Logger.getLogger(MyClass.class.getName()); 
+}
--- src/ca/mcgill/cs/comp303/rummy/model/Logger.java
+++
@@ -1,9 +0,0 @@
-package ca.mcgill.cs.comp303.rummy.model;
-
-import java.util.logging.*;
-
-public class Logger
-{
-	
-	private final static Logger LOGGER = Logger.getLogger(MyClass.class.getName()); 
-}
