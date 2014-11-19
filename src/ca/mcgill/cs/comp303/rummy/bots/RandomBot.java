b3371c3c81f8a264cc905011330654a5183065a2
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2014-11-18 20:47:25 -0500

    updated randombot

--- src/ca/mcgill/cs/comp303/rummy/bots/RandomBot.java
+++ src/ca/mcgill/cs/comp303/rummy/bots/RandomBot.java
@@ -2,8 +2,13 @@ package ca.mcgill.cs.comp303.rummy.bots;
 
 import java.util.Random;
 
+import ca.mcgill.cs.comp303.rummy.exceptions.CannotDrawException;
+import ca.mcgill.cs.comp303.rummy.exceptions.CannotPerformActionException;
 import ca.mcgill.cs.comp303.rummy.model.Card;
+import ca.mcgill.cs.comp303.rummy.model.GameEngine;
+import ca.mcgill.cs.comp303.rummy.model.GameEngine.GamePhase;
 import ca.mcgill.cs.comp303.rummy.model.Hand;
+import ca.mcgill.cs.comp303.rummy.model.Player;
 
 /**
  * Random bot does all random moves and knocks when possible.
@@ -13,6 +18,8 @@ public class RandomBot implements RummyBot
 {
 	private Hand aHand;
 	private Random aRandom = new Random();
+	private boolean aFirstCall = false;
+	private Player aSelf;
 	
 	/**
 	 * Default constructor. 
@@ -21,35 +28,80 @@ public class RandomBot implements RummyBot
 	public RandomBot(Hand pHand)
 	{
 		this.aHand = pHand;
+		this.aSelf = GameEngine.getInstance().getPlayer2();
 	}
 
 	@Override
-	public boolean drawFromDeck()
+	public void gameUpdate(GamePhase pCurrentPhase, Card pCard)
 	{
-		return this.aRandom.nextBoolean();
+		// If the AIs turn just started.
+		if(pCurrentPhase.equals(GamePhase.AI_TURN) && !aFirstCall)
+		{
+			playTurn();
+		}
+		
+		// Turn just started
+		if(pCurrentPhase.equals(GamePhase.FIRSTDRAW))
+		{
+			aSelf = GameEngine.getInstance().getPlayer2();
+			aHand = aSelf.getHand();
+		}
 	}
 
 	@Override
-	public Card discard()
+	public void gameEnded(boolean pP1Knocked, boolean pP1Won, boolean pEndByDeckShortage, int pPointsGained)
 	{
+		// do nothing if the game ended
+	}
+
+	@Override
+	public void playTurn()
+	{
+		// Draw randomly if the deck size doesn't end in game over
+		if(GameEngine.getInstance().getDeckSize() > 2 && aRandom.nextBoolean())
+		{
+			GameEngine.getInstance().drawFromDeck(aSelf);
+		}
+		else
+		{
+			try
+			{
+				GameEngine.getInstance().drawFromDiscardPile(aSelf);
+			}
+			catch (CannotDrawException e)
+			{				
+				GameEngine.getInstance().drawFromDeck(aSelf);
+			}
+		}
+		
+		// Discard randomly and knock
 		Card[] unmatched = (Card[]) this.aHand.getUnmatchedCards().toArray();
+		Card discardCard;		
 		if(unmatched.length == 0)
 		{
-			return unmatched[this.aRandom.nextInt() % unmatched.length];
+			discardCard = unmatched[this.aRandom.nextInt() % unmatched.length];
 		}
-		
-		int randomIndex = this.aRandom.nextInt() % this.aHand.size();
-		return this.aHand.getCards().get(randomIndex);
-	}
-
-	@Override
-	public boolean knock()
-	{
-		if(this.aHand.score() <= 10) 
+		else
 		{
-			return true;
+			int randomIndex = this.aRandom.nextInt() % this.aHand.size();
+			discardCard = this.aHand.getCards().get(randomIndex);			
 		}
 		
-		return false;
+		boolean canKnock = false;
+		if(this.aHand.score() <= GameEngine.HAND_SIZE) 
+		{
+			canKnock = true;
+		}
+		
+		// now do the actual draw action
+		try
+		{
+			GameEngine.getInstance().discard(aSelf, discardCard, canKnock);
+		}
+		catch (CannotPerformActionException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}	
 }
